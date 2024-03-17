ca9f97909173e171345b331aa65ba57cb6eb3fc1
peixed351 <peixed351@gmail.com>
2024-03-17 12:55:49 +0300

    add settings

--- src/convert.py
+++ src/convert.py
@@ -1,9 +1,15 @@
+import csv
 import os
 import shutil
+import xml.etree.ElementTree as ET
+from collections import defaultdict
 
 import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
 from supervisely.io.fs import (
+    dir_exists,
     file_exists,
+    get_file_ext,
     get_file_name,
     get_file_name_with_ext,
     get_file_size,
@@ -11,7 +17,6 @@ from supervisely.io.fs import (
 from tqdm import tqdm
 
 import src.settings as s
-from dataset_tools.convert import unpack_if_archive
 
 
 def convert_and_upload_supervisely_project(
@@ -19,72 +24,81 @@ def convert_and_upload_supervisely_project(
 ) -> sly.ProjectInfo:
     # Possible structure for bbox case. Feel free to modify as you needs.
 
-    root_path = ""
-    images_folder = "images"
-    bboxes_folder = "labels"
+    dataset_path = "/home/alex/DATASETS/TODO/palm trees/Palm-Counting-349images"
     batch_size = 30
-    img_ext = ".png"
-    ann_ext = ".txt"
+    anns_ext = ".xml"
+    ann_ext = "_labels.csv"
 
     def create_ann(image_path):
-        labels, img_tags, label_tags = [], [], []
+        labels = []
 
-        image_np = sly.imaging.image.read(image_path)[:, :, 0]
-        img_height = image_np.shape[0]
-        img_width = image_np.shape[1]
+        ann_path = image_path.split(".")[0] + anns_ext
 
-        file_name = get_file_name(image_path)
-        curr_anns_dirpath = ""
-        ann_path = os.path.join(curr_anns_dirpath, file_name + ann_ext)
+        tree = ET.parse(ann_path)
+        root = tree.getroot()
 
-        if file_exists(ann_path):
-            with open(ann_path) as f:
-                content = f.read().split("\n")
-                for curr_data in content:
-                    if len(curr_data) != 0:
-                        curr_data = list(map(float, curr_data.split(" ")))
+        img_height = int(root.find(".//height").text)
+        img_wight = int(root.find(".//width").text)
 
-                        left = int((curr_data[1] - curr_data[3] / 2) * img_width)
-                        right = int((curr_data[1] + curr_data[3] / 2) * img_width)
-                        top = int((curr_data[2] - curr_data[4] / 2) * img_height)
-                        bottom = int((curr_data[2] + curr_data[4] / 2) * img_height)
+        if img_height == 3648 or img_height == 2160:
+            drone = sly.Tag(drone_a)
+        else:
+            drone = sly.Tag(drone_b)
 
-                        rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+        file_name = get_file_name_with_ext(image_path)
 
-                        for obj_class in obj_classes:
-                            if obj_class.name == idx2clsname[curr_data[0]]:
-                                curr_obj_class = obj_class
-                                break
-                        label = sly.Label(rectangle, curr_obj_class, label_tags)
-                        labels.append(label)
+        bboxes_data = name_to_boxes[file_name]
+        for coords in bboxes_data:
+            left = int(coords[0])
+            top = int(coords[1])
+            right = int(coords[2])
+            bottom = int(coords[3])
+            rect = sly.Rectangle(left=left, top=top, right=right, bottom=bottom)
+            label = sly.Label(rect, obj_class)
+            labels.append(label)
 
-        return sly.Annotation(img_size=(img_height, img_width), labels=labels, img_tags=img_tags)
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=[drone])
 
-    class_names = ["class1", "class2", ...]
-    idx2clsname = {}
-    obj_classes = [sly.ObjClass(name, sly.Rectangle) for name in class_names]
+    obj_class = sly.ObjClass("palm tree", sly.Rectangle, color=(255, 0, 0))
+    drone_a = sly.TagMeta("DJI Phantom 4 Pro dron", sly.TagValueType.NONE)
+    drone_b = sly.TagMeta("DJI Mavic Pro dron", sly.TagValueType.NONE)
 
     project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
-    meta = sly.ProjectMeta(obj_classes=obj_classes)
+    meta = sly.ProjectMeta(obj_classes=[obj_class], tag_metas=[drone_a, drone_b])
     api.project.update_meta(project.id, meta.to_json())
 
-    for ds_name in os.listdir(root_path):
-        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
-        dataset_path = os.path.join(root_path, ds_name)
+    for ds_name in os.listdir(dataset_path):
 
-        images_pathes = sly.fs.list_files_recursively(dataset_path, valid_extensions=[img_ext])
+        data_path = os.path.join(dataset_path, ds_name)
 
-        pbar = tqdm(desc=f"Create dataset '{ds_name}'", total=len(images_pathes))
-        for images_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
-            images_names_batch = [
-                get_file_name_with_ext(image_path) for image_path in images_pathes_batch
+        if dir_exists(data_path):
+
+            dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+            name_to_boxes = defaultdict(list)
+            ann_path = data_path + ann_ext
+            with open(ann_path, "r") as file:
+                csvreader = csv.reader(file)
+                for row in csvreader:
+                    name_to_boxes[row[0]].append(row[4:])
+
+            images_names = [
+                im_name for im_name in os.listdir(data_path) if get_file_ext(im_name) != anns_ext
             ]
 
-            img_infos = api.image.upload_paths(dataset.id, images_names_batch, images_pathes_batch)
-            img_ids = [image.id for image in img_infos]
+            progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
 
-            anns = [create_ann(image_path) for image_path in images_pathes_batch]
-            api.annotation.upload_anns(img_ids, anns)
+            for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+                img_pathes_batch = [
+                    os.path.join(data_path, image_name) for image_name in images_names_batch
+                ]
 
-            pbar.update(len(images_names_batch))
+                img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+                img_ids = [im_info.id for im_info in img_infos]
+
+                anns = [create_ann(image_path) for image_path in img_pathes_batch]
+                api.annotation.upload_anns(img_ids, anns)
+
+                progress.iters_done_report(len(images_names_batch))
+
     return project
--- src/settings.py
+++ src/settings.py
@@ -13,31 +13,31 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Palm Trees"
+PROJECT_NAME_FULL: str = "Palm Trees Dataset"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Unknown()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.SearchAndRescue()]
+CATEGORY: Category = Category.Safety(extra=[Category.Aerial(), Category.Drones()])
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+RELEASE_DATE: Optional[str] = "2020-05-11"  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
     RELEASE_YEAR: int = None
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/riotulab/aerial-images-of-palm-trees"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 16041748
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/palm-trees"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
@@ -46,26 +46,26 @@ GITHUB_URL: str = None
 DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
-CLASS2COLOR: Optional[Dict[str, List[str]] | Literal["predefined"]] = "predefined"
+CLASS2COLOR: Optional[Dict[str, List[str]] or Literal["predefined"]] = "predefined"
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = "https://arxiv.org/pdf/2005.05269"
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {
-    "GitHub": "some_link_to_repo_if_exists"
-}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = ["Adel Ammar", "Anis Koubaa"]
 AUTHORS_CONTACTS: Optional[List[str]] = None
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "Prince Sultan University, Saudi Arabia"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://www.psu.edu.sa/"
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {
+    "dorne types": ["DJI Phantom 4 Pro dron", "DJI Mavic Pro dron"]
+}
 TAGS: Optional[
     List[
         Literal[
